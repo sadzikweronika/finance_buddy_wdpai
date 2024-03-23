1df47bba74ff029446f66b0dd80ff17219f051e1
Weronika Sadzik <63665547+sadzikweronika@users.noreply.github.com>
2024-03-23 09:01:39 +0100

    Added lab2

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+require_once 'src/controllers/AppControler.php';
+$controller = new AppController();
 
-echo 'Hi there! 😊';
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+$action = explode("/", $path)[0];
+$controller->render($action);
