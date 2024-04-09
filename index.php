bf7c89415e89a0842af2518a3af004d702640794
Weronika Sadzik <63665547+sadzikweronika@users.noreply.github.com>
2024-04-09 20:54:10 +0200

    Basic routing and User model

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
-require_once 'src/controllers/AppControler.php';
+require_once 'src/controllers/AppController.php';
 require_once 'src/models/Project.php';
+require 'Routing.php';
 
-$controller = new AppController();
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
-$action = explode("/", $path)[0];
-switch($action){
-    case "dashboard":
-        $projectA = new Project("A", "Sample desc", "https://picsum.photos/300/200");
-        $projects = [$projectA];
-        $controller->render($action, ["items" => $projects]);
-        break;
-    case "projects":
-        $controller->render($action);
-        break;
-    default:
-        $controller->render($action);
-    }
+
+Routing::get('index', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+Routing::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'AppController.php';
 class DefaultController extends AppController{
+    public function index() {
+        $this->render('login');
+    }
 
+    public function dashboard() {
+        $this->render('dashboard');
+    }
 }
